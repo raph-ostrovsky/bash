b89133ca7bdc5db1af942a805a2591d486f4d72b
Rafael <89470801+raph-ostrovsky@users.noreply.github.com>
2022-05-27 11:19:18 +0200

    Create 2_spec_var_pseudo.sh

---
+++ 2_spec_var_pseudo.sh
@@ -0,0 +1,25 @@
+```bash
+#!/bin/bash
+
+  
+
+# Display the UID
+
+echo "Your UID is ${UID}"
+
+
+# Display the username
+
+USER_NAME=$(id -un)
+echo "Your username is ${USER_NAME}"
+
+
+# Display if the user is root or not
+
+if [[ "${UID}" -eq 0 ]]
+then
+echo 'You are root'
+else
+echo 'You are not root'
+fi
+```
